60cac1bd80a3c3e1ce43fd9d3494c1a97cf2062a
EsauRoque <55461753+EsauRoque@users.noreply.github.com>
2019-09-21 17:58:27 -0500

    Add files via upload

---
+++ ejercicio02.py
@@ -0,0 +1,7 @@
+#Ejercicio 2. Hola.
+#Escriba un programa que le pida al usuario que ingrese su nombre. 
+#El programa debe responder con un mensaje que diga hola al usuario,
+#usando su nombre.
+
+nombre=input('Cual es tu nombre:')
+print('Hola',nombre, 'mucho gusto')

c915016e934a8ee508a5bcd9fdc5110de1c4f298
EsauRoque <55461753+EsauRoque@users.noreply.github.com>
2019-09-21 17:37:32 -0500

    Add files via upload

---
+++ ejercicio01.py
@@ -0,0 +1,18 @@
+#Ejercicio 1 Dirección de envío
+#Cree un programa que muestre su nombre y la dirección postal completa formateada en
+#la forma en que normalmente lo verías en el exterior de un sobre. Su programa
+#no necesita leer ninguna entrada del usuario.
+
+print('Alberto Roque')
+print('Retorno de Naranjos 9A')
+print('Colona. San Bartolome Coatepec')
+print('Huixquilucan')
+print('Mexico')
+print('')
+print('')
+print('')
+print('')
+print('                                  Dionicio Ramirez')
+print('                                  Calle Castro 32')
+print('                                  Sierra Madre Oaxaca')
+print('                                  Mexico')
